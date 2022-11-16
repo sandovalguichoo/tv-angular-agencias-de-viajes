1a730b78645d1e8ec9420a79e9c1132c547581a7
Sandoval Güicho <sandovalguichoo@gmail.com>
2022-11-16 09:14:23 -0600

    modulo de agencias listo y limpio el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ModuloMenuModule } from './modulo-menu/modulo-menu.module';
 import { ModuloFooterModule } from './modulo-footer/modulo-footer.module';
-import { ModuloToursMexicoModule } from './modulo-tours-mexico/modulo-tours-mexico.module';
 import { ModuloTravelingMexicoModule } from './modulo-traveling-mexico/modulo-traveling-mexico.module';
-import { ModuloComparadorDeViajesModule } from './modulo-comparador-de-viajes/modulo-comparador-de-viajes.module';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -18,9 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     ModuloMenuModule,
     ModuloFooterModule,
-    ModuloToursMexicoModule,
     ModuloTravelingMexicoModule,
-    ModuloComparadorDeViajesModule,
     HttpClientModule,
     BrowserModule
   ],
--- src/app/modulo-menu/modulo-menu.module.ts
+++ src/app/modulo-menu/modulo-menu.module.ts
@@ -4,13 +4,9 @@ import { MenuPrincipalComponent } from './menu-principal/menu-principal.componen
 import { RouterModule, Routes } from '@angular/router';
 import { VistaPanelAdministracionTravelingMexicoComponent } from '../modulo-traveling-mexico/vista-panel-administracion-traveling-mexico/vista-panel-administracion-traveling-mexico.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { VistaPanelAdministracionToursMexicoComponent } from '../modulo-tours-mexico/vista-panel-administracion-tours-mexico/vista-panel-administracion-tours-mexico.component';
-import { VistaPaginaInicioComponent } from '../modulo-comparador-de-viajes/vista-pagina-inicio/vista-pagina-inicio.component';
 
 const routes: Routes = [
   { path: 'traveling-mexico', component: VistaPanelAdministracionTravelingMexicoComponent },
-  {path:'tours-mexico',component: VistaPanelAdministracionToursMexicoComponent},
-  {path:'comparador-de-viajes', component: VistaPaginaInicioComponent}
 ]
 
 @NgModule({
